a7d0289c8d2d64f2b7d00edbfa9c5195bca08b09
Joy <github.joysaha@gmail.com>
2024-04-22 09:18:34 +0600

    add coffee section edits

--- src/Components/AddCoffee/AddCoffee.jsx
+++ src/Components/AddCoffee/AddCoffee.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from "react";
 
 const AddCoffee = () => {
-    return (
-        <div>
-            <h2>Hello add coffee</h2>
+  return (
+    <div className="bg-[#F4F3F0] p-24">
+      <h2 className="text-4xl font-bold">Add coffee</h2>
+      <form className="text-center w-2/4">
+        <div className="md:flex">
+          <div className="form-control md:w-1/2 ">
+            <label className="label">
+              <span className="label-text">Coffee Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Coffee Name"
+              className="input input-bordered w-full"
+              required
+            />
+          </div>
+          <div className="form-control md:w-1/2 ml-4">
+            <label className="label">
+              <span className="label-text">Available Quantity</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Available Quantity"
+              className="input input-bordered w-full"
+              required
+            />
+          </div>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default AddCoffee;
